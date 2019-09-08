34ff75f624db20ff1d290348e397e9ff896df863
tjm2285 <tjm2285@yahoo.com>
2019-09-08 08:03:27 -0700

    set up visual studio solution

---
+++ DesignPatterns/DesignPatterns/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace DesignPatterns
+{
+    public class Demo
+    {
+        static void Main(string[] args)
+        {
+            
+            WriteLine("Helo World!!!!");
+
+            
+        }
+    }
+}
